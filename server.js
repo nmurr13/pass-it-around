68ddbef2b837a63d7276d8f9e55540290ab61ea1
Nicholas Murray <nmurr13@protonmail.com>
2023-12-19 12:25:12 -0500

    pass it around

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send(`<h1>99 Bottles of Beer on the Wall!</h1>
+    <a href = "/98">Take one down & pass it around!</a>`)
+})
+
+app.get('/:number_of_bottles', (req, res) => {
+    const number_of_bottles = req.params.number_of_bottles
+    if (number_of_bottles > 0) {
+    res.send(`<h1>${number_of_bottles} Bottles of beer on the wall</h1>
+    <a href = "/${number_of_bottles -1}">Take one down, pass it around<a/>`)}
+    else {
+        res.send(`<a href="./"><1>Hair of the Dog</1></a>`);
+    }
+})
+
+app.listen(3000, () => {console.log("server is listening on port 3000")})
